3c827e218d02537ec03d24d9687229136dbb61e9
Adson Gomes Oliveira <adsongoliveira2022@outlook.com>
2023-04-07 00:21:00 -0300

    <feat> Created route for create a default invoice

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require('express-async-errors');
 const express = require('express');
 const helmet = require('helmet');
 const paymentRoutes = require('./routes/payment.routes');
+const invoiceRoutes = require('./routes/invoice.routes');
 const errorMiddleware = require('./middlewares/error.middleware');
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(helmet());
 
 app.get('/health-check', (_req, res) => res.status(200).send('Connection OK'));
 app.use('/api/payments', paymentRoutes);
+app.use('/api/invoices', invoiceRoutes);
 app.use(errorMiddleware);
 
 module.exports = app;
