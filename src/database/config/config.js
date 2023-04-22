e6428dff5a0a30324bb17ba496af863baa1d2cdf
Adson Gomes Oliveira <adsongoliveira2022@outlook.com>
2023-04-22 09:51:57 -0300

    <setup> Adding env configs for deploy

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
     username: process.env.DB_USER || 'root',
@@ -16,10 +18,11 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    username: 'root',
-    password: 'null',
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
   },
 };
--- src/services/createPaymentByOrder.consumer.service.js
+++ src/services/createPaymentByOrder.consumer.service.js
@@ -1,9 +1,12 @@
 const amqplib = require('amqplib');
 const PaymentsServices = require('./payments.service');
 const sendPaymentToInvoice = require('./sendPaymentToInvoice.producer.service');
+require('dotenv').config();
+
+const RABBIT_MQ_CONNECT = process.env.RABBIT_STRING || 'amqp://guest:guest@rabbit-ms-gateway:5672';
 
 const createPaymentByOrder = async (queue, exchange) => {
-  const connectionMQ = await amqplib.connect('amqp://guest:guest@rabbit-ms-gateway:5672');
+  const connectionMQ = await amqplib.connect(RABBIT_MQ_CONNECT);
   const consumerChannel = await connectionMQ.createChannel();
   await consumerChannel.assertExchange(exchange, 'fanout', { durable: true });
   await consumerChannel.assertQueue(queue, { durable: true });
